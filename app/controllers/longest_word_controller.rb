33a06db51e5f3cfab3dc8ff5fcee0ceff39bc99b
Moritz Wahrlich <mwahrlich@users.noreply.github.com>
2017-08-14 16:47:34 +0200

    sessions

--- app/controllers/longest_word_controller.rb
+++ app/controllers/longest_word_controller.rb
@@ -5,6 +5,8 @@ class LongestWordController < ApplicationController
   end
 
   def score
+    session[:times_played] ? session[:times_played] += 1 : session[:times_played] = 1
+    @times_played = session[:times_played]
     @answer = params[:answer].downcase
     @start = params[:start].to_i
     @grid = params[:grid].split(",")
@@ -18,5 +20,8 @@ class LongestWordController < ApplicationController
     @result = { message: "Well done", score: ((2**@answer.length.to_f / (@result[:time].to_f))*10).round(2), time: @result[:time] }
     @result = { message: "Not an english word", score: 0 } if dict_result["found"] == false
     @result = { message: "Word is not in the grid", score: 0 } if array != @answer.upcase.split("")
+    session[:total] ? session[:total] += @result[:score] : session[:total] = 0
+    @total = session[:total]
+    @average = (@total / @times_played).round(2)
   end
 end
